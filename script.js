0a1815a2b1fe6c7f3f3b2c4c7f46344eead68d6a
Sarthak <sarthakdhoke132@gmail.com>
2023-08-07 17:44:58 +0530

    created hackerMan

---
+++ script.js
@@ -0,0 +1,61 @@
+let a = [
+    "Connecting to instagram...",
+    'password = password.strip()',
+    "seeking for the server...",
+    "Connecting to server 1...",
+    "id has been fatched successfully!",
+    'password',
+    "**************",
+    "Connection failed. Retrying...",
+    'try:',
+    '95135 attempts. The password is pass',    "chars = string.printable.strip()",
+    "Connecting to server 2",
+    'password',
+    "**************",
+    'try:',
+    "Connected Successfully...",
+    "Username **********...", 
+    "Trying Brute Force...",
+    "**************",
+    "**************",
+    "200K passwords tried...",
+    "Match not found",
+    "Another 100K passwords tried...",
+    'def bruteCracking(username,error):',
+    "**************",
+    "Match not found...",
+    "Another 200K passwords tried...",
+    "Match not found...",
+    "**************",
+    "Username **********...",   
+    "**************",
+    "**************",
+    'except:',
+    'def bruteCracking(username,url,error):',
+    "Accessing Account...",
+    "got the username and password!",
+    "Hack Successful...",
+    "operating system is crashed",
+    "your Instagram id and password is hacked",
+    "your Instagram id and password is hacked",
+    "your Instagram id and password is hacked",
+    "your Instagram id and password is hacked",
+]
+
+const sleep = async (seconds) => {
+    return new Promise((resolve, reject) => {
+            setTimeout(() => { resolve(true) }, seconds * 280)
+    })
+}
+
+const showHack = async (message) => {
+    await sleep(2)
+    // console.log(message)
+    text.innerHTML = text.innerHTML + message + "<br>"
+}
+
+(async () => {
+    for (let i = 0; i < a.length; i++) {
+            await showHack(a[i])
+    }
+})()
